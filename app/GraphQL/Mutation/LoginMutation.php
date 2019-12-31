db5d0514b1552d24f3af2f38b3d24b2b4fd84978
lucas.santos <lucas.santos@kbrtec.com.br>
2019-12-31 06:26:54 -0300

    Ajuste no login

--- app/GraphQL/Mutation/LoginMutation.php
+++ app/GraphQL/Mutation/LoginMutation.php
@@ -9,7 +9,6 @@ use GraphQL\Type\Definition\Type;
 use GraphQL\Type\Definition\ResolveInfo;
 use Rebing\GraphQL\Support\SelectFields;
 use Rebing\GraphQL\Support\Mutation;
-use JWTAuth;
 
 class LoginMutation extends Mutation
 {
@@ -39,21 +38,6 @@ class LoginMutation extends Mutation
         ];
     }
 
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    /**
-     * Return a key value array, containing any custom claims to be added to the JWT.
-     *
-     * @return array
-     */
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
-
     public function resolve($root, $args, $context, ResolveInfo $resolveInfo, Closure $getSelectFields)
     {
         $credentials = [
@@ -61,7 +45,7 @@ class LoginMutation extends Mutation
             'password' => $args['password']
         ];
 
-        $token = JWTAuth::attempt($credentials);
+        $token = auth()->attempt($credentials);
 
         if (!$token) {
             throw new \Exception('Unauthorized!');
